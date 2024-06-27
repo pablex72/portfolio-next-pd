78c04c762fb88992f9e73392d2ba76b4a2ebe9fb
pablo <tonyp2hoy@gmail.com>
2024-06-27 14:21:16 -0400

    hero: image and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TypeWriteEffect from "./Helper/TypeWriteEffect";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -26,11 +27,14 @@ const Hero = () => {
               <button className="relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
                 <span className="relative z-10">Hire me!</span>
               </button>
-              <button className="before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:-rotate-180">
+              <button className="before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
                 <span className="relative z-10">Download CV</span>
               </button>
             </div>
           </div>
+          <div className="hidden md:block">
+            <Image src="/images/hero.png" alt="hero" width={600} height={600} className="object-contain"/>
+            </div>
         </div>
       </div>
     </div>
